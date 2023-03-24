bdf97f1cf5103b2c6b90dce297a6219400dc688f
km <kyawmyo.oo@outlook.com>
2023-03-24 21:02:15 +0800

    add headers to fix CORS error
    fix syntax error

--- functions/basic-api.js
+++ functions/basic-api.js
@@ -5,6 +5,10 @@ const data = require("../assets/data");
 exports.handler = async (event, context) => {
   console.log(event);
   return {
+    // to fix cors error, just need to add headers
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     statusCode: 200,
     body: JSON.stringify(data),
   };
