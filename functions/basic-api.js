447507a5e32fbe4a81baddae09141ed4c4a9e416
km <kyawmyo.oo@outlook.com>
2023-03-24 21:06:03 +0800

    Revert "add headers to fix CORS error"
    This reverts commit bdf97f1cf5103b2c6b90dce297a6219400dc688f.

--- functions/basic-api.js
+++ functions/basic-api.js
@@ -5,10 +5,6 @@ const data = require("../assets/data");
 exports.handler = async (event, context) => {
   console.log(event);
   return {
-    // to fix cors error, just need to add headers
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
     statusCode: 200,
     body: JSON.stringify(data),
   };
